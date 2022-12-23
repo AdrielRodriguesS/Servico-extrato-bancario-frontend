6b6e6e2eb84384f4bc4c88b1d8b1ef74757563a2
Adriel Rodrigues <adriels.projetos@gmail.com>
2022-12-22 22:32:35 -0300

    integração front-back e campos principais

--- src/components/Body/transacoes.tsx
+++ src/components/Body/transacoes.tsx
@@ -1,36 +1,119 @@
-import './styles.css'
-import axios from 'axios'
-import { useState } from 'react'
-import { Conta } from '../../models/Conta'
+import "./styles.css";
+import axios from "axios";
+import { useState } from "react";
+import { Conta } from "../../models/Conta";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css'
+import { Transferencia } from "../../models/Transferencia";
+import { BASE_URL } from "../../utils/request";
 
-function Transacoes () {
+function Transacoes() {
+  const [idConta, setIdConta] = useState("");
+  const [conta, setConta] = useState<Conta>({ id: "", nomeResponsavel: "" });
+  const [msg, setMsg] = useState("");
 
-    const [idConta, setIdConta] = useState('')
-    const [conta, setConta] = useState<Conta>({"id":"", "nomeResponsavel" : ""})
-    const [msg, setMsg] = useState("")
+  const min = new Date();
+  const max = new Date();
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
+  const [nomeOp, setNomeOp] = useState("");
 
-    return(
-        <div className="main-card" translate="no">
-            <h2 className="main-title">Transações</h2>
-            <div className="card-principal">
-                <div className="campos-principais">
-                    <div className="nomes-campos">Id / nº da Conta</div>
-                    <div>
-                        <input
-                        className="id-field"
-                        type="text"
-                        onChange={(e) => setIdConta(e.target.value)}
-                        onFocus={(e) => {conta.nomeResponsavel = "", setMsg('')}}
-                        ></input>
-                    </div>
-                </div>
-                <div className="campos-principais">
-                    <div className="nome-titular">{conta.nomeResponsavel}</div>
-                </div>
+  const [totalConta, setTotalConta] = useState(0.0);
+  let total = 0;
+  const [transferencias, setTransferencias] = useState<Transferencia[]>([]);
+  for (const transf of transferencias) {
+    total += transf.valor;
+  }
+
+  function handleClick() {
+    if (idConta != "") {
+      axios(`${BASE_URL}/contas/${idConta}`).then((response) => {
+        setConta(response.data);
+      });
+      axios
+        .get(`${BASE_URL}/transferencias/${idConta}/total-valores`)
+        .then((response) => {
+          setTotalConta(response.data);
+        });
+    } else {
+      setMsg("Digite um id válido");
+    }
+  }
+
+  return (
+    <div className="main-card" translate="no">
+      <h2 className="main-title">Transações</h2>
+      <div className="card-principal">
+        <div className="campos-principais">
+          <div className="nomes-campos">Id / nº da Conta</div>
+          <div>
+            <input
+              className="id-field"
+              type="text"
+              onChange={(e) => setIdConta(e.target.value)}
+              onFocus={(e) => {
+                (conta.nomeResponsavel = ""), setMsg("");
+              }}
+            ></input>
+          </div>
+        </div>
+        <div className="campos-principais">
+          <div className="nome-titular">{conta.nomeResponsavel}</div>
+        </div>
+      </div>
+      <div className="msg">{msg}</div>
+
+      <div className="card-transacoes">
+        <div className="campos">
+          <div className="nomes-campos">Data de Início</div>
+          <div className="form-control-container">
+            <DatePicker
+              selected={minDate}
+              onChange={(date: Date) => setMinDate(date)}
+              className="form-control"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+        </div>
+        <div className="campos">
+          <div className="nomes-campos">Data de Fim</div>
+          <div className="form-control-container">
+            <DatePicker
+              selected={maxDate}
+              onChange={(date: Date) => setMaxDate(date)}
+              className="form-control"
+              dateFormat="dd/MM/yyyy"
+            />
+          </div>
+        </div>
+        <div className="campos">
+          <div className="nomes-campos">Nome Operador da Transação</div>
+          <div>
+            <input
+              className="id-nomeOp"
+              type="text"
+              onChange={(e) => setNomeOp(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className="red-btn-container">
+          <div className="red-btn" onClick={() => handleClick()}>
+            Buscar
+          </div>
+        </div>
+      </div>
+        <div className="card-transacoes">
+            <div className="campos">
+                <div className="nomes-campos">Saldo Total</div>
+                <div className="id-saldo">R$ {totalConta}</div>
+            </div>
+            <div className="campos">
+                <div className="nomes-campos">Saldo no Período</div>
+                <div className="id-saldo">R$ {total}</div>
             </div>
         </div>
-    )
-
+    </div>
+  );
 }
 
-export default Transacoes
+export default Transacoes;
